1b7dc70044dd6026ca880791cc5c0ffec263f7a9
HamaJjigae <166758235+HamaJjigae@users.noreply.github.com>
2024-09-24 23:28:09 -0600

    initial commit adds week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function StudentInfo()
+  {
+    return(
+      <div>
+        <p>Matthew Biggs</p>
+          <Link href="https://github.com/HamaJjigae/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+            https://github.com/HamaJjigae/cprg306-assignments
+          </Link>
+      </div>
+    );
+  }
