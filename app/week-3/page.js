d03e274860ca8f7c0241d381353d484d52d63014
HamaJjigae <166758235+HamaJjigae@users.noreply.github.com>
2024-10-02 15:31:28 -0600

    week-3 commit

---
+++ app/week-3/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ItemList from './item-list.js';
+
+const Page = () =>
+  {
+    return (
+      <main className="min-h-screen bg-black text-green-500 p-8">
+        <h1 className="text-3x1 font-bold mb-8 text-center">Shopping List</h1>
+        <ItemList />
+      </main>
+    );
+  };
+
+export default Page;
